001a8475ac3effb5c6d852b6a61412e367fa06ce
Fabriciasouzalima <fabriciasouzalima@gmail.com>
2019-09-22 10:29:47 -0300

    modificar funções no data, incluir sort e filter

--- src/data.js
+++ src/data.js
@@ -1,18 +1,25 @@
 function getValue (arr, key, value) {
-  return arr.filter( persona => persona[key] === value );
+  return arr.filter(persona => persona[key] === value).sort((a,b)=>a.name>b.name? 1:-1);
+}
+
+function getPerson (data,search){
+  return data.filter(person => person.name.toUpperCase().includes(search))
 }
 
 function getAllName(data) {
   let allName = []
-  for (let elements of data) {
-    allName.push(elements.data)
+  if (name === "A-Z") {
+    allName = data.sort((a, b) => a.name > b.name ? 1 : -1);
+  } else if (name === "Z-A") {
+    allName = data.sort((a, b) => a.name > b.name ? -1 : 1);
   }
-  return (allName.sort());
+  return allName
 }
 
 
 window.data = {
   getValue,
-  getAllName
+  getAllName,
+  getPerson
 }
 
