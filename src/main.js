bb201fb90d762db7b39fd48dd772e2a9d44d8ea0
sshregina <sheila.regina.94@gmail.com>
2019-09-25 13:53:30 -0300

    alteracoes de css

--- src/main.js
+++ src/main.js
@@ -19,7 +19,7 @@ document.getElementById("btn-order-za").addEventListener("click", allNameZtoA);
 
 function buildHtmlPerc(obj) {
   let html = "";
-  obj.items.forEach((item) => html += `
+  obj.itens.forEach((item) => html += `
   <div class="card">
     <div class="card-inner">
       <div class="card-front">
@@ -39,81 +39,68 @@ function buildHtmlPerc(obj) {
       </div>
   </div>
   `);
-  myDiv.innerHTML = `A porcentagem de personagens com essas características é ${obj.perc}%`;
-  tela.innerHTML = html ;
-}
-
-function buildHtml(itens) {
-  let html = "";
-  itens.items.forEach((item) => html += `
-  <div class="card">
-    <div class="card-inner">
-      <div class="card-front">
-        <img src="${item.image}">
-        <p>Nome: <strong>${item.name}</strong></p>
-      </div>
-        <div class="card-back">
-          <ul class="card__list">
-            <li>Nome: <strong>${item.name}</strong></li>
-            <li>Status: <strong>${item.status}</strong></li>
-            <li>Espécie: <strong>${item.species}</strong></li>
-            <li>Gênero: <strong>${item.gender}</strong></li>
-            <li>Origem: <strong>${item.origin.name}</strong></li>
-            <li>Localização: <strong>${item.location.name}</strong></li>
-          </ul>
-        </div>
-      </div>
-  </div>
-  `);  
+  myDiv.innerHTML = `${obj.perc}% dos personagens possuem essas características.`;
   tela.innerHTML = html ;
 }
 
 function searchPersons() {
   let search = input.value.toUpperCase();
-  let name = window.data.getPerson(data, search);
-  return buildHtml(name);
+  let name = {"itens": window.data.getPerson(data, search)};
+  myDiv.classList.add("invisible");
+  return buildHtmlPerc(name);
 }
 
 function filterFemale() {
+  myDiv.classList.remove("invisible");
   buildHtmlPerc(window.data.getValue(data, "gender", "Female"));
 }
 
 function filterMale() {
+  myDiv.classList.remove("invisible");
   buildHtmlPerc(window.data.getValue(data, "gender", "Male"));
 }
 
 function filterGenderUnknown() {
+  myDiv.classList.remove("invisible");
   buildHtmlPerc(window.data.getValue(data, "gender", "unknown"));
 }
 
 function filterDead() {
+  myDiv.classList.remove("invisible");
   buildHtmlPerc(window.data.getValue(data, "status", "Dead"));
 }
 
 function filterAlive() {
+  myDiv.classList.remove("invisible");
   buildHtmlPerc(window.data.getValue(data, "status", "Alive"));
 }
 
 function filterStatusUnknown() {
+  myDiv.classList.remove("invisible");
   buildHtmlPerc(window.data.getValue(data, "status", "unknown"));
 }
 
 function filterSpecieHuman() {
+  myDiv.classList.remove("invisible");
   buildHtmlPerc(window.data.getValue(data, "species", "Human"));
 }
 
 function filterSpecieAlien() {
-  builHtmlPerc(window.data.getValue(data, "species", "Alien"));
+  myDiv.classList.remove("invisible");
+  buildHtmlPerc(window.data.getValue(data, "species", "Alien"));
 }
 
 function filterSpecieHumanoid() {
-  builHtmlPerc(window.data.getValue(data, "species", "Humanoid"));
+  myDiv.classList.remove("invisible");
+  buildHtmlPerc(window.data.getValue(data, "species", "Humanoid"));
 }
 
 function allNameAtoZ() {
-  buildHtml(window.data.getAllName(data, "A-Z"));
+  myDiv.classList.add("invisible");
+  buildHtmlPerc({"itens": window.data.getAllName(data, "A-Z")});
 }
 
 function allNameZtoA() {
-  buildHtml(window.data.getAllName(data, "Z-A"));
+  myDiv.classList.add("invisible");
+  buildHtmlPerc({"itens": window.data.getAllName(data, "Z-A")});
 }
